acc4b875963bae0b0f10b49efb876f3df8aeecef
venu <vguntupalli@techarrows.com>
2019-12-31 23:16:44 -0500

    Initial version

---
+++ config-client-app/src/main/java/com/venuguntupalli/springcloud/configclientapp/ConfigClientAppApplication.java
@@ -0,0 +1,37 @@
+package com.venuguntupalli.springcloud.configclientapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConfigClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAppApplication.class, args);
+	}
+
+	@Autowired
+	private ConfigClientAppConfiguration properties;
+
+	@Value("${some.other.property}")
+	private String someOtherProperty;
+
+	@RequestMapping
+	public String printConfig(){
+		StringBuffer sb = new StringBuffer();
+		sb.append(properties.getProperty());
+		sb.append(" || ");
+		sb.append(someOtherProperty);
+		return sb.toString();
+	}
+
+}
